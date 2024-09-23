b45201240f95544422a1242f77c4b0a273d97f7f
hanlinniu <niuhanlin@icloud.com>
2024-09-23 14:13:07 +0100

    update

--- source/extensions/omni.isaac.lab_tasks/omni/isaac/lab_tasks/manager_based/manipulation/z1_lift_from_drawer/config/z1/joint_pos_env_cfg.py
+++ source/extensions/omni.isaac.lab_tasks/omni/isaac/lab_tasks/manager_based/manipulation/z1_lift_from_drawer/config/z1/joint_pos_env_cfg.py
@@ -67,7 +67,7 @@ class Z1CubeLiftEnvCfg(Z1LiftEnvCfg):
             prim_path="{ENV_REGEX_NS}/Object",
             init_state=RigidObjectCfg.InitialStateCfg(
                 pos=[0.3, 0, 0.75], rot=[0.7071068, -0.7071068, 0, 0]
-            ),  # rot=[0.7071068, -0.7071068, 0, 0]
+            ),  
             spawn=UsdFileCfg(
                 # usd_path=os.path.join(os.path.expanduser("~"), "Downloads/YCB/Axis_Aligned/006_mustard_bottle.usd"),
                 usd_path=f"{ISAAC_NUCLEUS_DIR}/Props/Blocks/DexCube/dex_cube_instanceable.usd",
