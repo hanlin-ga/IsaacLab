edc66f500dcc5e369c795c1f59a8f15925a3fc92
hanlin-ga <hanlin@generalautonomy.in>
2024-10-14 13:24:09 +0000

    update

--- source/extensions/omni.isaac.lab_tasks/omni/isaac/lab_tasks/manager_based/manipulation/z1_lift_from_table/mdp/rewards.py
+++ source/extensions/omni.isaac.lab_tasks/omni/isaac/lab_tasks/manager_based/manipulation/z1_lift_from_table/mdp/rewards.py
@@ -95,11 +95,12 @@ def object_goal_distance_six_joint(
     distance = torch.norm(des_pos_w - object.data.root_pos_w[:, :3], dim=1)
 
     angle = asset.data.joint_pos[:, robot_cfg.joint_ids] - asset.data.default_joint_pos[:, robot_cfg.joint_ids]
+    print("angle is ", angle)
     # print("default_joint_pos is ", asset.data.joint_pos[:, robot_cfg.joint_ids])
     # return torch.sum(torch.abs(angle[:,0:6]), dim=1)
     # print("(object.data.root_pos_w[:, 2] > minimal_height) * ((1 - torch.tanh(distance / std)) - torch.sum(torch.abs(angle[:,0:6]), dim=1)*0.1) is ", (object.data.root_pos_w[:, 2] > minimal_height) * ((1 - torch.tanh(distance / std)) - torch.sum(torch.abs(angle[:,0:6]), dim=1)*0.1))
     # rewarded if the object is lifted above the threshold
-    return (object.data.root_pos_w[:, 2] > minimal_height) * ((1 - torch.tanh(distance / std)) - torch.sum(torch.abs(angle[:,0:6]), dim=1)*0.1)
+    return (object.data.root_pos_w[:, 2] > minimal_height) * ((1 - torch.tanh(distance / std)) - torch.sum(torch.abs(angle[:,0:6]), dim=1)*0.1 - torch.abs(angle[:,5])*1)
 
 
 def last_joint_vel(env, asset_cfg: SceneEntityCfg = SceneEntityCfg("robot")) -> torch.Tensor:
